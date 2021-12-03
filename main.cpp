d5e1dc9600e7bcfa590dfe013e055e6bb212d061
Vladislav_Karasev <vladkar55@gmail.com>
2021-12-03 20:36:44 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-double my_pow(double a, unsigned int b)
+double my_pow(double a, int b)
 {
     double temp = a;
     if (b == 0)
         return 1;
     for (int i = 0; i < b-1; i++)
     {
-       temp = temp * a;
+        temp = temp * a;
     }
+    if (b < 0)
+    {
+        temp = 1 / temp;
+        return temp;
+    }
+    return temp;
 }
